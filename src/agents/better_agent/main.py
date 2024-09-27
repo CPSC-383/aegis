3453c3bb873e9479d93a786f9d45f5b145019050
Dante <dkirsman@gmail.com>
2024-09-27 10:39:33 -0600

    refactor(log level): set better aagent log level to error

--- src/agents/better_agent/main.py
+++ src/agents/better_agent/main.py
@@ -7,10 +7,10 @@ from agent.log_levels import LogLevels
 
 def main() -> None:
     if len(sys.argv) == 1:
-        BaseAgent.set_log_level(LogLevels.All)
+        BaseAgent.set_log_level(LogLevels.Error)
         BaseAgent.get_base_agent().start_test(BetterAgent())
     elif len(sys.argv) == 2:
-        BaseAgent.set_log_level(LogLevels.All)
+        BaseAgent.set_log_level(LogLevels.Error)
         BaseAgent.get_base_agent().start_with_group_name(sys.argv[1], BetterAgent())
     else:
         print("Agent: Usage: python3 agents/better_agent/main.py <groupname>")
