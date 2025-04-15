f1d1e17f56af520c334f897e6d2fad875b67a004
Dante <dkirsman@gmail.com>
2025-04-14 18:35:35 -0600

    feat(ui ): add a button, and fix key issue

--- docs/components/Navbar.tsx
+++ docs/components/Navbar.tsx
@@ -52,8 +52,8 @@ export default function Navbar() {
           );
         })}
       </div>
-      <ThemeToggle />
       <Search />
+      <ThemeToggle />
     </nav>
   );
 }
--- docs/components/Search.tsx
+++ docs/components/Search.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { searchIndex } from "@/lib/search-index";
+import { cn } from "@/lib/utils";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 import {
@@ -10,8 +11,9 @@ import {
   CommandItem,
   CommandEmpty,
 } from "@/components/ui/command";
-import { DialogTitle } from "./ui/dialog";
+import { DialogTitle } from "@/components/ui/dialog";
 import { CommandGroup } from "cmdk";
+import { Button } from "@/components/ui/button";
 
 export default function Search() {
   const [open, setOpen] = useState(false);
@@ -29,7 +31,7 @@ export default function Search() {
     return () => document.removeEventListener("keydown", down);
   }, []);
 
-  const temp = searchIndex.flatMap((doc) => {
+  const attributes = searchIndex.flatMap((doc) => {
     const q = query.toLowerCase();
 
     const matchingAttributes = doc.attributes?.filter((attr) => {
@@ -52,35 +54,51 @@ export default function Search() {
   });
 
   return (
-    <CommandDialog open={open} onOpenChange={setOpen}>
-      <DialogTitle></DialogTitle>
-      <CommandInput
-        placeholder="Search docs..."
-        value={query}
-        onValueChange={setQuery}
-      />
-      <CommandList>
-        <CommandEmpty>No results found.</CommandEmpty>
-        {temp.map((group, i) => (
-          <CommandGroup key={`${group.name}-${i}`} heading={group.name}>
-            {group.items.map((attr) => (
-              <CommandItem
-                onSelect={() => {
-                  router.push(group.slug);
-                  setOpen(false);
-                }}
-              >
-                <div>
-                  <div className="font-medium">{attr.name}</div>
-                  <div className="text-sm text-muted-foreground">
-                    {attr.description}
+    <>
+      <Button
+        variant="outline"
+        className={cn(
+          "relative h-8 w-full justify-start rounded-[0.5rem] bg-muted/50 text-sm font-normal text-muted-foreground shadow-none sm:pr-12 md:w-40 lg:w-56 xl:w-64",
+        )}
+        onClick={() => setOpen(true)}
+      >
+        <span className="hidden lg:inline-flex">Search documentation...</span>
+        <span className="inline-flex lg:hidden">Search...</span>
+        <kbd className="pointer-events-none absolute right-[0.3rem] top-[0.3rem] hidden h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium opacity-100 sm:flex">
+          <span className="text-xs">⌘</span>K
+        </kbd>
+      </Button>
+      <CommandDialog open={open} onOpenChange={setOpen}>
+        <DialogTitle></DialogTitle>
+        <CommandInput
+          placeholder="Search docs..."
+          value={query}
+          onValueChange={setQuery}
+        />
+        <CommandList>
+          <CommandEmpty>No results found.</CommandEmpty>
+          {attributes.map((group) => (
+            <CommandGroup key={group.slug} heading={group.name}>
+              {group.items.map((attr, i) => (
+                <CommandItem
+                  key={`${group.slug}-${attr.name}-${i}`}
+                  onSelect={() => {
+                    router.push(group.slug);
+                    setOpen(false);
+                  }}
+                >
+                  <div>
+                    <div className="font-medium">{attr.name}</div>
+                    <div className="text-sm text-muted-foreground">
+                      {attr.description}
+                    </div>
                   </div>
-                </div>
-              </CommandItem>
-            ))}
-          </CommandGroup>
-        ))}
-      </CommandList>
-    </CommandDialog>
+                </CommandItem>
+              ))}
+            </CommandGroup>
+          ))}
+        </CommandList>
+      </CommandDialog>
+    </>
   );
 }
