0cbc145c445f35ec5f1bb614bfb5bbcf39e3bb7c
Dante <dkirsman@gmail.com>
2025-06-11 15:43:41 -0600

    fix(sidebar): more minimal, didnt like the block and borders arounde each item

--- docs/components/Sidebar.tsx
+++ docs/components/Sidebar.tsx
@@ -17,10 +17,10 @@ export default function Sidebar({ items, className = "", onItemClick }: Props) {
       <div className="overflow-y-auto h-full no-scrollbar">
         <nav>
           {items.map((item, index) => (
-            <div key={index} className="border-b border-zinc-200 dark:border-zinc-800">
+            <div key={index}>
               {!item.disabled && (
-                <div className="p-4 bg-zinc-100 dark:bg-zinc-900 border-b border-zinc-200 dark:border-zinc-800">
-                  <h4 className="text-xs font-mono uppercase tracking-wider font-black">
+                <div className="p-4">
+                  <h4 className="text-sm font-mono uppercase tracking-wider font-black">
                     {item.title}
                   </h4>
                 </div>
@@ -31,22 +31,16 @@ export default function Sidebar({ items, className = "", onItemClick }: Props) {
                     subItem.href && !subItem.disabled ? (
                       <Link key={subIndex} href={subItem.href}>
                         <div
-                          className={`group px-4 py-3 text-sm font-mono border-b border-zinc-100 dark:border-zinc-900 transition-colors cursor-pointer relative ${pathname === subItem.href + "/"
-                            ? "bg-zinc-300 dark:bg-zinc-800 text-foreground dark:text-foreground font-bold"
-                            : "text-zinc-600 dark:text-zinc-400 hover:bg-zinc-50 dark:hover:bg-zinc-950 hover:text-black dark:hover:text-white"
+                          className={`group px-4 py-3 text-sm font-mono transition-colors cursor-pointer relative ${pathname === subItem.href + "/"
+                            ? "bg-zinc-100 dark:bg-zinc-900 text-foreground dark:text-foreground font-medium"
+                            : "text-zinc-600 dark:text-zinc-400 hover:bg-zinc-100 dark:hover:bg-zinc-900 hover:text-black dark:hover:text-white"
                             }`}
                           onClick={onItemClick}
                         >
-                          <div className="flex items-center justify-between">
+                          <div className="flex items-center">
                             <span className="uppercase tracking-wide text-xs">
                               {subItem.title}
                             </span>
-                            {pathname === subItem.href + "/" && (
-                              <div className="w-2 h-2 bg-zinc-950 dark:bg-zinc-200"></div>
-                            )}
-                            {pathname !== subItem.href + "/" && (
-                              <div className="h-px w-2 bg-zinc-800 origin-right scale-x-100 group-hover:scale-x-150 transition-transform duration-300" />
-                            )}
                           </div>
                         </div>
                       </Link>
