059a7b95fd30b141e80a2a6c7526d7ebc2d5b2cb
Dante <dkirsman@gmail.com>
2025-07-10 15:00:00 -0600

    chore: 🧹 add agent commands in aegis/__init__.py

--- src/_aegis/common/commands/agent_commands/__init__.py
+++ src/_aegis/common/commands/agent_commands/__init__.py
@@ -5,7 +5,7 @@ __all__ = [
     "MOVE",
     "OBSERVE",
     "SAVE_SURV",
-    "PREDICT",
+    # "PREDICT",
     "SEND_MESSAGE",
     "SLEEP",
     "TEAM_DIG",
@@ -17,7 +17,8 @@ from _aegis.common.commands.agent_commands.CONNECT import CONNECT
 from _aegis.common.commands.agent_commands.END_TURN import END_TURN
 from _aegis.common.commands.agent_commands.MOVE import MOVE
 from _aegis.common.commands.agent_commands.OBSERVE import OBSERVE
-from _aegis.common.commands.agent_commands.PREDICT import PREDICT
+
+# from _aegis.common.commands.agent_commands.PREDICT import PREDICT
 from _aegis.common.commands.agent_commands.SAVE_SURV import SAVE_SURV
 from _aegis.common.commands.agent_commands.SEND_MESSAGE import SEND_MESSAGE
 from _aegis.common.commands.agent_commands.SLEEP import SLEEP
--- src/aegis/__init__.py
+++ src/aegis/__init__.py
@@ -1,3 +1,27 @@
 from _aegis.cli import main
+from _aegis.common.commands.agent_commands import (
+    AGENT_UNKNOWN,
+    END_TURN,
+    MOVE,
+    OBSERVE,
+    # PREDICT,
+    SAVE_SURV,
+    SEND_MESSAGE,
+    SLEEP,
+    TEAM_DIG,
+)
 
-__all__ = ["main"]
+# To generate the list, use `uv run python -c` or just `python -c` :
+# python -c 'import aegis; names = sorted(n for n in dir(aegis) if not n.startswith("_")); print("__all__ = [\n    \"" + "\",\n    \"".join(names) + "\",\n]")'
+
+__all__ = [
+    "AGENT_UNKNOWN",
+    "END_TURN",
+    "MOVE",
+    "OBSERVE",
+    "SAVE_SURV",
+    "SEND_MESSAGE",
+    "SLEEP",
+    "TEAM_DIG",
+    "main",
+]
