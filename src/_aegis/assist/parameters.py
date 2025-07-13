aa1341427ba5c566c64aff44609f6a3cbeb20324
Dante <dkirsman@gmail.com>
2025-07-13 14:19:30 -0600

    chore: 🧹 remove unused vars

--- src/_aegis/assist/parameters.py
+++ src/_aegis/assist/parameters.py
@@ -3,8 +3,6 @@ from dataclasses import dataclass
 
 @dataclass
 class Parameters:
-    milliseconds_to_wait_for_agent_command: int = int(1.25 * 1000)
-    milliseconds_to_wait_for_agent_connect: int = 5 * 1000
     number_of_rounds: int = 500
     number_of_agents: int = 0
     world_filename: str = "ExampleWorld.world"
